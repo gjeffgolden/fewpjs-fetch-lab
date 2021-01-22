70efd015fce122776bef3f7c83ecfdb4e4a60523
gjeffgolden <gjgolden08@gmail.com>
2021-01-22 15:54:40 -0700

    Passing.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(books => renderBooks(books))
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main')
   books.forEach(book => {
+    console.log(book)
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
